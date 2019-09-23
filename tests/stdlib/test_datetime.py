68436f149870335ca806e55d52dd8d74b8e008a0
Andreas Olofsson <ao@blazar.se>
2019-09-23 15:11:53 +0200

    Fixed tests in `tests.stdlib.test_datetime`.

--- tests/stdlib/test_datetime.py
+++ tests/stdlib/test_datetime.py
@@ -17,20 +17,20 @@ class test_datetime(NotImplementedToExpectedFailure, TranspileTestCase):
         self.assertCodeExecution("""
         import datetime
         print(datetime.timedelta(1,2,3))
-        print(datetime.timedelta(week=-3,day=2))
-        print(datetime.timedelta(years=-100,microseconds=2))
+        print(datetime.timedelta(weeks=-3,days=2))
+        print(datetime.timedelta(weeks=-100,microseconds=2))
         print(datetime.timedelta(-1,100,321,-100000,20,13,2))
-        print(datetime.timedelta(days=-2,years=1))
-         """)
+        print(datetime.timedelta(days=-2,weeks=1))
+        """)
 
 
     def test_constructor_invalid(self):
         self.assertCodeExecution("""
         import datetime
         try:
-            print(datetime.timedelta("string")
-            print(datetime.timedelta(1, day=1)
-            print(datetime.timedelta(a=1)
+            print(datetime.timedelta("string"))
+            print(datetime.timedelta(1, day=1))
+            print(datetime.timedelta(a=1))
         except Exception as e:
             print(e)      
         """)
@@ -46,9 +46,9 @@ class test_datetime(NotImplementedToExpectedFailure, TranspileTestCase):
     def test_attributes_instance (self):
         self.assertCodeExecution("""
         import datetime
-        print(datetime.timedelta(7).days
-        print(datetime.timedelta(0,7).seconds
-        print(datetime.timedelta(0,0,7).microseconds
+        print(datetime.timedelta(7).days)
+        print(datetime.timedelta(0,7).seconds)
+        print(datetime.timedelta(0,0,7).microseconds)
         """)
 
 
