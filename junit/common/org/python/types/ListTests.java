162444930759b42977cce205cd33facae0e321df
Andreas Olofsson <ao@blazar.se>
2019-10-01 16:05:55 +0200

    Added start of `insert` junit test for `List`.

--- junit/common/org/python/types/ListTests.java
+++ junit/common/org/python/types/ListTests.java
@@ -8,7 +8,18 @@ import org.junit.jupiter.api.DisplayName;
 class ListTests {
     @Test
     @DisplayName("TEST")
-    void testTest() {
-        assertEquals(1, 1);
+    void testInsert() {
+        org.python.types.List x = new org.python.types.List(
+            new java.util.ArrayList(java.util.Arrays.asList(
+                org.python.types.Int.getInt(1),
+                org.python.types.Int.getInt(2),
+                org.python.types.Int.getInt(3)
+            ))
+        );
+        x.insert(
+            org.python.types.Int.getInt(0),
+            org.python.types.Int.getInt(4)
+        );
+        assertEquals(x.__str__().toString(), "[4, 1, 2, 3]");
     }
 }
