08ed3e080c243bb75ec9c4d6ba89de7bf8078724
Andreas Olofsson <ao@blazar.se>
2019-10-01 14:33:30 +0200

    Added junit test stub.

---
+++ junit/common/org/python/types/ListTests.java
@@ -0,0 +1,14 @@
+package org.python.types;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+class ListTests {
+    @Test
+    @DisplayName("TEST")
+    void testTest() {
+        assertEquals(1, 1);
+    }
+}
