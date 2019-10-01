ee4efb9218fb35665cc8ec27c0445ba493019441
Valter Gådin <valter94@live.se>
2019-10-01 16:59:19 +0200

    Added test for List, extend with list

--- junit/common/org/python/types/ListTests.java
+++ junit/common/org/python/types/ListTests.java
@@ -2,8 +2,12 @@ package org.python.types;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.Python;
 import org.junit.jupiter.api.Test;
+import org.python.Object;
 import org.junit.jupiter.api.DisplayName;
+
+
 
 class ListTests {
     @Test
@@ -11,4 +15,28 @@ class ListTests {
     void testTest() {
         assertEquals(1, 1);
     }
+
+    @Test
+    @DisplayName("Extend List")
+    void testExtendList() {
+        java.util.ArrayList<org.python.Object> testList = new java.util.ArrayList<org.python.Object>();
+        testList.add(Int.getInt(1));
+        testList.add(Int.getInt(2));
+        testList.add(Int.getInt(3));
+
+        // Create [1,2,3]
+        org.python.types.List pythonList = new org.python.types.List(testList);
+        // Extend to [1,2,3,1,2,3]
+        pythonList.extend(pythonList);
+
+        testList.add(Int.getInt(1));
+        testList.add(Int.getInt(2));
+        testList.add(Int.getInt(3));
+
+        // Create list [1,2,3,1,2,3]
+        org.python.types.List pythonList2 = new org.python.types.List(testList);
+
+        assertEquals(pythonList2.__str__(), pythonList.__str__());
+    }
+
 }
