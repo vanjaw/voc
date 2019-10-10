07b0d8ff2b5f100069398be49f5831fa943d4948
vanjaw <vanja.wallner@me.com>
2019-10-10 12:47:13 +0200

    updated workload

--- workload/python/remove.py
+++ workload/python/remove.py
@@ -3,12 +3,14 @@ import time
 def test_remove():
     my_list = list()
 
-    for i in range(50000000):
+    for i in range(30000000):
         my_list.append(1)
 
     my_list.append(2)
+    my_list.append(2)
 
     start = time.time()
+    my_list.remove(2)
     my_list.remove(2)
     finish = time.time()
     return (finish-start)
