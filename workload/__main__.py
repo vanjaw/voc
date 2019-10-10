ba0921a77ad4724f11234b96394908ccd9a78caa
vanjaw <vanja.wallner@me.com>
2019-10-10 11:19:00 +0200

    remove-workload

--- workload/__main__.py
+++ workload/__main__.py
@@ -1,12 +1,14 @@
 import slice
 import sort
 import reverse
+import remove
 
 if __name__ == '__main__':
     tests = {
         'slice': slice.test_slice,
         'sort':  sort.test_sort,
-        'reverse': reverse.test_reverse 
+        'reverse': reverse.test_reverse,
+        'remove': remove.test_remove
     }
 
     width = max([len(a) for a in tests]) + 2
--- workload/remove.py
+++ workload/remove.py
@@ -1,14 +1,17 @@
 import time
 
-my_list = list()
+def test_remove():
+    my_list = list()
 
-for i in range(30000000):
-    my_list.append(1)
+    for i in range(50000000):
+        my_list.append(1)
 
-my_list.append(2)
+    my_list.append(2)
 
-start = time.time()
-my_list.remove(2)
-finish = time.time()
-print("Time: ", finish - start)
+    start = time.time()
+    my_list.remove(2)
+    finish = time.time()
+    return (finish-start)
 
+if __name__ == '__main__':
+    print('Duration: {} s'.format(test_remove()))
