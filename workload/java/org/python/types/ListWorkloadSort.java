5963b8287c04455376da84b55c0c082fee586fd5
Tyson McLeod <tyson.mcleod.8851@student.uu.se>
2019-10-16 17:07:27 +0200

    Update ListWorkloadSort.java

--- workload/java/org/python/types/ListWorkloadSort.java
+++ workload/java/org/python/types/ListWorkloadSort.java
@@ -1,9 +1,27 @@
 package org.python.types;
 
 import org.python.types.List;
+import java.util.Random;
 
 public class ListWorkloadSort {
+    private static int list_size = 1000000;
+
     public static void workload() {
         // Workload here
+
+        List test_list = new List();
+
+        Random rand = new Random();
+
+        for(int i = 0; i < list_size; i++){
+          int randint = rand.nextInt(100);
+          test_list.append(Int.getInt(randint));
+        }
+        /*
+        long start = System.currentTimeMillis();
+        test_list.sort(null,null);
+        long finish = System.currentTimeMillis();
+        System.out.println("Duration: " + (start-finish));
+        */
     }
 }
