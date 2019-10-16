1544646f46306c5a79fa499f078e290566b887eb
Valter Gådin <valter94@live.se>
2019-10-16 15:59:40 +0200

    Done with reverse workload

--- workload/java/org/python/types/ListWorkloadReverse.java
+++ workload/java/org/python/types/ListWorkloadReverse.java
@@ -1,13 +1,12 @@
 package org.python.types;
 
-import org.Python;
-
 public class ListWorkloadReverse {
     public static void workload() {
-        org.python.types.Range range = new org.python.types.Range(10000000);
-        org.python.types.List list = new org.python.types.List(range);
+        org.python.types.Range range = new org.python.types.Range(Int.getInt(10000));
+        org.python.Object[] objArray = {range};
+        org.python.types.List list = new org.python.types.List(objArray, null);
 
-        for(int i = 0; i<100; i++) {
+        for(int i = 0; i<10000; i++) {
             list.reverse();
         }
     }
